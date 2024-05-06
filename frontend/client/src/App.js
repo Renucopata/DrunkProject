ce2cc840aed3aac69ed800c987f47a7a877b0b67
Juan Cla C <juanclaudiocarrascotapia@gmail.com>
2024-05-06 19:53:39 -0400

    Commit add login y ruteo correcto

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
 import ConvocatoriaPage from './pages/ConvocatoriaPage';
 import HistorialPage from './pages/HistorialPage'; // Importa aquí tu HistorialPage
@@ -20,6 +21,7 @@ function App() {
           <Route path="/historial" element={isLoggedIn ? <HistorialPage /> : <Navigate replace to="/login" />} /> {/* Ruta para HistorialPage con protección de autenticación */}
           <Route path="/convocatoria" element={isLoggedIn ? <ConvocatoriaPage /> : <Navigate replace to="/login" />} /> {/* Protección de autenticación también añadida aquí */}
           <Route path="/" element={isLoggedIn ? <DashboardPage /> : <Navigate replace to="/login" />} />
+          <Route path="/registro" element={!isLoggedIn ? <RegisterPage /> : <Navigate replace to="/" />} />
           {/* Agrega aquí otras rutas según sea necesario */}
         </Routes>
       </div>
