0cd58e34bbd128db82ba49b7e888500127df38ce
Rene Rengel <Rene.Rengel@jala.university>
2024-05-05 23:12:34 -0400

    "dash back"

--- backend/routes/jwtAuth.js
+++ backend/routes/jwtAuth.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const bcrypt = require("bcrypt");
-
-
 const db = require('../db');
 
 //const jwtGenerator = require("../utils/jwtGenerator");
