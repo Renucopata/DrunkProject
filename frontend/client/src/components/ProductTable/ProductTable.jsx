1e95850dc933cb344c98d69500658222846e69c2
Rene Rengel <Rene.Rengel@jala.university>
2024-05-08 23:32:11 -0400

    thead fixed

--- frontend/client/src/components/ProductTable/ProductTable.jsx
+++ frontend/client/src/components/ProductTable/ProductTable.jsx
@@ -19,11 +19,20 @@ const ProductTable = ({ convocatorias }) => {
 
     return (
         <div className="convocatoria-table relative overflow-x-auto">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                {/* Table headers */}
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    {/* Table header rows */}
-                </thead>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" className="px-6 py-3">Id</th>
+                    <th scope="col" className="px-6 py-3">Requerimiento</th>
+                    <th scope="col" className="px-6 py-3">Fecha de Apertura</th>
+                    <th scope="col" className="px-6 py-3">Fecha de Cierre</th>
+                    <th scope="col" className="px-6 py-3">Estado</th>
+                    <th scope="col" className="px-6 py-3">Owner</th>
+                    <th scope="col" className="px-6 py-3">Postulante Elegido</th>
+                    <th scope="col" className="px-6 py-3">Categoria</th>
+                    <th scope="col" className="px-6 py-3">Acción</th>
+                </tr>
+            </thead>
                 <tbody>
                     {/* Table rows */}
                     {convocatorias.map(convocatoria => (
