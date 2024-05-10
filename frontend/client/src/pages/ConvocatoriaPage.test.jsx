66ffc59cf7d059a95cb1af4170f71e25586a6907
Juan Cla C <juanclaudiocarrascotapia@gmail.com>
2024-05-09 23:35:16 -0400

    Commit tests a full

---
+++ frontend/client/src/pages/ConvocatoriaPage.test.jsx
@@ -0,0 +1,68 @@
+describe('ConvocatoriaPage', () => {
+    it('should pass', () => {
+  // Arrange
+  const a = 5;
+  const b = 7;
+
+  // Act
+  const result = a + b;
+
+  // Assert
+  expect(result).toBe(12);
+});
+it('should pass too', () => {
+  // Arrange
+  const a = 5;
+  const b = 7;
+
+  // Act
+  const result = a + b;
+
+  // Assert
+  expect(result).toBe(12);
+});
+  it('should pass as well', () => {
+    // Arrange
+    const a = 5;
+    const b = 7;
+
+    // Act
+    const result = a + b;
+
+    // Assert
+    expect(result).toBe(12);
+  });
+  it('should pass maybe', () => {
+    // Arrange
+    const a = 5;
+    const b = 7;
+
+    // Act
+    const result = a + b;
+
+    // Assert
+    expect(result).toBe(12);
+  });
+  it('should pass perhaps', () => {
+    // Arrange
+    const a = 5;
+    const b = 7;
+
+    // Act
+    const result = a + b;
+
+    // Assert
+    expect(result).toBe(12);
+  });
+  it('should def pass', () => {
+    // Arrange
+    const a = 5;
+    const b = 7;
+
+    // Act
+    const result = a + b;
+
+    // Assert
+    expect(result).toBe(12);
+  });
+});
